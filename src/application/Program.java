13736ad8929d042e6097e53aa63d86ed61747ab7
gabrielafreitasilva <gabi-1518@hotmail.com>
2023-01-12 19:52:06 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,67 @@
+package application;
+
+import java.time.LocalDate;
+
+import entities.Bootcamp;
+import entities.Curso;
+import entities.Dev;
+import entities.Mentoria;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Curso curso1 = new Curso();
+		curso1.setTitulo("curso java");
+		curso1.setDescricao("descricao curso java");
+		curso1.setCargaHoraria(8);
+
+		Curso curso2 = new Curso();
+		curso2.setTitulo("curso js");
+		curso2.setDescricao("descricao curso js");
+		curso2.setCargaHoraria(4);
+
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria de java");
+		mentoria.setDescricao("descricao mentoria de java");
+		mentoria.setData(LocalDate.now());
+
+		/*
+		 * System.out.println(curso1); System.out.println(curso2);
+		 * System.out.println(mentoria);
+		 */
+
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+
+		Dev devAmanda = new Dev();
+		devAmanda.setNome("Amanda");
+		devAmanda.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Amanda: " + devAmanda.getConteudosInscritos());
+		devAmanda.progredir();
+		devAmanda.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Amanda: " + devAmanda.getConteudosInscritos());
+		System.out.println("Conteúdos Concluídos Amanda: " + devAmanda.getConteudosConcluidos());
+		System.out.println("XP: " + devAmanda.calcularTotalXp());
+
+		System.out.println("------");
+
+		Dev devArley = new Dev();
+		devArley.setNome("Arley");
+		devArley.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Arley: " + devArley.getConteudosInscritos());
+		devArley.progredir();
+		devArley.progredir();
+		devArley.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Arley: " + devArley.getConteudosInscritos());
+		System.out.println("Conteúdos Concluídos Arley: " + devArley.getConteudosConcluidos());
+		System.out.println("XP:" + devArley.calcularTotalXp());
+
+	}
+}
